bc3209311770b80ce2a83ce22d6d1e1f3ddc3668
XerOssa <jacek.ossowski95.@gmail.com>
2025-02-24 21:22:48 +0100

    hardhat

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+
+module.exports = {
+  solidity: "0.8.28",
+  networks: {
+    bsc: {  
+      url: 
+        "https://bsc-dataseed.binance.org/",
+        accounts: [process.env.PRIVATE_KEY],
+    },  
+    bsc_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
